f0d4e023e1ea3dd574cd7b5adc7401589db40c16
timaevV <timaevvladislav@gmail.com>
2023-12-12 22:49:06 +0600

    feat:modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core"
+import { CommonModule } from "@angular/common"
+import { RouterOutlet } from "@angular/router"
+
+import {SelectorComponent} from "./selector/selector.component"
+import {CalendarComponent} from "./calendar/calendar.component"
+import {OrganizerComponent} from "./organizer/organizer.component"
+import {MomentPipe} from "./shared/moment.pipe"
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    SelectorComponent,
+    CalendarComponent,
+    OrganizerComponent,
+    MomentPipe
+  ],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.scss"
 })
-export class AppComponent {
-  title = 'organizer';
-}
+
+export class AppComponent { title = "organizer" }
