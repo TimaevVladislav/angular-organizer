44ae31bb06ec5acaf015a8d43c0d1b3637e49e1e
timaevV <timaevvladislav@gmail.com>
2023-12-18 23:41:49 +0600

    feat:tasks logic render

--- src/app/organizer/organizer.component.ts
+++ src/app/organizer/organizer.component.ts
@@ -13,10 +13,17 @@ import {Task, TasksService} from "../shared/tasks.service"
 export class OrganizerComponent implements OnInit {
 
   form: FormGroup = new FormGroup({})
+  tasks: Task[] = []
 
   constructor(protected dateService: DateService, protected tasksService: TasksService) {}
 
   ngOnInit() {
+    this.dateService.date.pipe(
+      switchMap(value => this.tasksService.fetchTasks(value))
+    ).subscribe(tasks => {
+      this.tasks = tasks
+    })
+
      this.form = new FormGroup({
        title: new FormControl("", Validators.required)
      })
@@ -27,7 +34,14 @@ export class OrganizerComponent implements OnInit {
      const task: Task = { title, date: this.dateService.date.value.format("DD-MM-YYYY") }
 
      this.tasksService.createTask(task).subscribe(task => {
-        this.form.reset()
+       this.tasks.push(task)
+       this.form.reset()
      }, err => console.error(err))
+  }
+
+  remove(task: Task) {
+    this.tasksService.remove(task).subscribe(() => {
+      this.tasks = this.tasks.filter(t => t.id !== task.id)
+    }, err => console.error(err))
   }
 }
