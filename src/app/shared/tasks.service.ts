fe38c666260924e1fb6af69ebd268afec20f3492
timaevV <timaevvladislav@gmail.com>
2023-12-18 23:41:02 +0600

    feat:remove task

--- src/app/shared/tasks.service.ts
+++ src/app/shared/tasks.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core"
 import { HttpClient } from "@angular/common/http"
 import { map, Observable } from "rxjs"
+import moment from "moment";
 
 export interface Task {
   id?: string
@@ -15,11 +16,27 @@ export class TasksService {
 
   constructor(private http: HttpClient) {}
 
+  fetchTasks(date: moment.Moment) {
+    return this.http
+      .get<Task[]>(`${TasksService.url}/${date.format("DD-MM-YYYY")}.json`)
+      .pipe(map(tasks => {
+         if (!tasks) {
+           return []
+         }
+
+         return Object.keys(tasks).map(key => ({...tasks[key], id: key}))
+      }))
+  }
+
   createTask(task: Task): Observable<Task> {
     return this.http
       .post<any>(`${TasksService.url}/${task.date}.json`, task)
       .pipe(map(res => {
         return { ...task, id: res.name }
       }))
+  }
+
+  removeTask(task: Task): Observable<void>{
+    return this.http.delete<void>(`${TasksService.url}/${task.date}/${task.id}.json`)
   }
 }
